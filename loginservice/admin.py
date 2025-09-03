6327a27911c1d5971c17fd4e013b07d63b20bbd0
JesusEmilianoCloud <al214801@alumnos.uacj.mx>
2025-09-03 00:35:22 -0500

    Fix import CustomUser

--- loginservice/admin.py
+++ loginservice/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+
+CustomUser = get_user_model()
 
 class CustomUserAdmin(UserAdmin):
     list_display = ["first_name", "last_name", "username", "email", "phone_number"]
