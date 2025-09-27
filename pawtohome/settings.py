9eedd139b9f64e76bd7e1508d4536bc85b708ad4
JesusEmilianoCloud <al214801@alumnos.uacj.mx>
2025-09-26 22:46:12 -0500

    Finish Google-Auth. Add load socialapp in template/login.html.

--- pawtohome/settings.py
+++ pawtohome/settings.py
@@ -162,21 +162,9 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 
 # Provider specific settings
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    }
-}
 
 #Basic Config.
 LOGIN_URL = 'loginservice:login-register'
