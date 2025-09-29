1f5eb799b276e5ce75a7456b72c3210e1cc65dba
JesusEmilianoCloud <al214801@alumnos.uacj.mx>
2025-09-29 18:37:54 -0500

    Add new url. And add new branch

--- ProfileService/urls.py
+++ ProfileService/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "ProfileService"
 
 urlpatterns = [
-   
+        path("<int:user_id>", views.getUserProfileData, name="profile"),    
     # Otras rutas de la aplicación
 ]
